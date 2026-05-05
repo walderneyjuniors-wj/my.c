d42ce0fc99e4a097f52df0b18042bde726f44406
walderneyjuniors-wj <202603546322@alunos.estacio.br>
2026-05-05 00:40:10 +0000

    Foram adicionados mais navios e criados loops for para colocá-los em posições diferetes no tabuleiro do jogo batalha naval

--- Jogo Batalha Naval.c
+++ Jogo Batalha Naval.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
+#define tam_navio 3
+
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
@@ -11,18 +15,7 @@ int main() {
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
     char coluna [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};  //Identificação das colunas
-    int linha [10][10] = { //Identificação das linhas com matriz bidimencional
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 3, 3, 3},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    int tabuleiro [linhas][colunas] = {0};  //Inicialização do tabuleiro com 0 (vazio)
     /*linha[2][3] = 3; // Horizontal
     linha[2][4] = 3;
     linha[2][5] = 3;
@@ -30,6 +23,30 @@ int main() {
     linha[5][8] = 3; // Vertical
     linha[6][8] = 3;
     linha[7][8] = 3;*/
+
+    // --- POSICIONAMENTO COM LOOPS FOR ---
+
+    // 1. Navio Vertical (Ex: Coluna 1, Linhas 2 a 4)
+    for (int i = 0; i < 0 + tam_navio; i++) {
+        tabuleiro[i][0] = 3;
+    }
+
+    // 2. Navio Horizontal (Ex: Linha 8, Colunas 4 a 6)
+    for (int j = 4; j < 4 + tam_navio; j++) {
+        tabuleiro[9][j] = 3;
+    }
+
+    // 3. Navio Diagonal Principal (Linha e Coluna aumentam juntas)
+    // Coordenada inicial: (0, 5) -> (1, 6) -> (2, 7)
+    for (int k = 0; k < tam_navio; k++) {
+        tabuleiro[2 + k][5 + k] = 3; // 
+    }
+
+    // 4. Navio Diagonal Secundária (Linha aumenta, Coluna diminui)
+    // Coordenada inicial: (5, 9) -> (6, 8) -> (7, 7)
+    for (int k = 0; k < tam_navio; k++) {
+        tabuleiro[5 + k][9 - k] = 3; // 
+    }
 
     printf("\n   ===   BEM VINDO A BATALHA NAVAL   ===   \n");   
     printf("\n");
@@ -42,11 +59,11 @@ int main() {
     }
     printf("\n");
     printf("    --------------------\n");
-    for (int c = 0; c < 10; c++) {   //Loop para gerar o tabuleiro
-        printf("%2d | ", c + 1);
+    for (int l = 0; l < 10; l++) {   //Loop para gerar o tabuleiro
+        printf("%2d | ", l + 1);
     
-        for (int l = 0; l < 10; l++) {
-            printf("%d ", linha[c][l]);
+        for (int c = 0; c < 10; c++) {
+            printf("%d ", tabuleiro[l][c]);
             
         }
         printf("\n");
